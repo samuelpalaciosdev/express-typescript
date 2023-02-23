21d002ac8511c79403e0a4ee60b2a8ba3c9dc274
Samuel <samuelpl0888@gmail.com>
2023-02-22 20:53:15 -0400

    Setup express project with typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.status(200).json({ msg: 'Hello world!' });
+  console.log('someone ping somehting here');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
